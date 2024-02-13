b02f13492d20d4552d7534f70f4590a1941349c7
unknown <shevchenkovlada623@gmail.com>
2024-02-13 20:30:07 +0200

    vlada

--- utils.py
+++ utils.py
@@ -7,3 +7,18 @@ def ncd(a, b):
 
 num1 = int(input())
 num2 = int(input())
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    elif number == 2:
+        return True
+    elif number % 2 == 0:
+        return False
+    else:
+        for i in range(3, int(number**0.5) + 1, 2):
+            if number % i == 0:
+                return False
+        return True
+
+
