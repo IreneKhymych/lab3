380528eb76c8e750b8f1d89d7b637f62193f14c5
idobrovolska <ilonadbrvlsk@gmail.com>
2024-02-14 18:52:32 +0200

    added a function call for palindrome

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ if result:
     print(number_to_check)
 else:
     print(number_to_check)
+
+
+input_str = input()
+result = is_palindrome(input_str)
+
+if result:
+    print(f"Послідовність '{input_str}' є паліндромом.")
+else:
+    print(f"Послідовність '{input_str}' не є паліндромом.")
