df97d04879fd0f1651e14d16c682882e60958110
idobrovolska <ilonadbrvlsk@gmail.com>
2024-02-14 18:57:34 +0200

    added column addition func

--- utils.py
+++ utils.py
@@ -26,3 +26,18 @@ def is_palindrome(s):
     cleaned_str = ''.join(char.lower() for char in s if char.isalnum())
     if cleaned_str == cleaned_str[::-1]:
         return True
+
+
+#for pull-request:
+def column_addition(num1, num2, perenis=0):
+    if num1 == 0 and num2 == 0 and perenis == 0:
+        return ""
+
+    prava_num_1 = num1 % 10
+    prava_num_2 = num2 % 10
+    current_sum = prava_num_1 + prava_num_2 + perenis
+    new_perenis = current_sum // 10
+    result_digit = current_sum % 10
+    remaining_sum = column_addition(num1 // 10, num2 // 10, new_perenis)
+
+    return str(remaining_sum) + str(result_digit)
